c789abc7dcf30f00d7afa2b3c4d79652774b3f78
Intakhab Alam Siddiqui <irin.irani@gmail.com>
2018-06-14 15:25:21 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+import { AlertComponent } from './directives/alert.component';
+import { AuthGuard } from './guards/auth.guard';
+import { JwtInterceptor,fakeBackendProvider} from './helpers';
+import { AlertService, AuthenticationService, UserService } from './services';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: JwtInterceptor,
+            multi: true
+        },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
